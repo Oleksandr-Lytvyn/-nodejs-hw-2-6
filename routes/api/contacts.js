5846098fc582942577cc2c000c2f0144ae29b35e
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-02-25 15:00:16 +0200

    point

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,12 +10,12 @@ const router = express.Router();
 
 router.get('/', ctrl.getList);
 
-router.get('/:contactId', ctrl.getById);
+// router.get('/:contactId', ctrl.getById);
 
-router.post('/', ctrl.add);
+// router.post('/', ctrl.add);
 
-router.delete('/:contactId', ctrl.del);
+// router.delete('/:contactId', ctrl.del);
 
-router.put('/:contactId', ctrl.update);
+// router.put('/:contactId', ctrl.update);
 
 module.exports = router;
